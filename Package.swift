c354e2885d2f5d4d6c0a64874a1034a739962435
Franklin Cruz <daisuke.sysoft@gmail.com>
2026-05-22 19:35:43 -0400

    feat(TASK-02): add SwiftLint config and SPM build plugin integration (#5)

--- Package.swift
+++ Package.swift
@@ -9,40 +9,48 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/rensbreur/SwiftTUI.git", from: "0.1.0"),
         .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.6.0"),
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.57.0"),
     ],
     targets: [
         .executableTarget(
             name: "emdee",
-            dependencies: ["Core", "TUIRenderer", "WebRenderer"]
+            dependencies: ["Core", "TUIRenderer", "WebRenderer"],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
         ),
         .target(
             name: "Core",
             dependencies: [
                 .product(name: "Markdown", package: "swift-markdown"),
-            ]
+            ],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
         ),
         .target(
             name: "TUIRenderer",
             dependencies: [
                 "Core",
                 .product(name: "SwiftTUI", package: "SwiftTUI"),
-            ]
+            ],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
         ),
         .target(
             name: "WebRenderer",
-            dependencies: ["Core"]
+            dependencies: ["Core"],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
         ),
         .testTarget(
             name: "CoreTests",
-            dependencies: ["Core"]
+            dependencies: ["Core"],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
         ),
         .testTarget(
             name: "TUIRendererTests",
-            dependencies: ["TUIRenderer"]
+            dependencies: ["TUIRenderer"],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
         ),
         .testTarget(
             name: "WebRendererTests",
-            dependencies: ["WebRenderer"]
+            dependencies: ["WebRenderer"],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
         ),
     ]
 )
